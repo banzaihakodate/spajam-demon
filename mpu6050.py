a2841a6b441642bc0598be331fb756c92f10a638
YuheiTakagawa <roto11fon@gmail.com>
2019-05-11 17:45:02 +0900

    fix: rename mpu6050

--- mpu6050.py
+++ mpu6050.py
@@ -57,15 +57,3 @@ class MPU6050:
 			ang_y = math.atan2(y, z) * 360 / 2.0 / math.pi
 			ang_z = math.atan2(x, y) * 360 / 2.0 / math.pi
 			return [ang_x, ang_y, ang_z]
-
-
-while True:
-        mpu = MPU6050()
-        acc_x, acc_y, acc_z = mpu.get_accel_data_g()
-        ang_x, ang_y, ang_z = mpu.calc_angle(acc_x, acc_y, acc_z)
-		if ang_y >= 0 or ang_y < -100:
-			print '%3d' % (ang_z % 360),
-		else:
-			print '%3d' % (-1 * ang_z % 360),
-        print
-	sleep(1)
